43bc0dfec664db16c84420ae087aaba2b6e6ed51
martinsnelson <nelsontecti@gmail.com>
2019-04-09 00:10:30 -0300

    Primeiro commit

--- TesteAutomatizadoSLPO/Program.cs
+++ TesteAutomatizadoSLPO/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TesteAutomatizadoSLPO
 {
     class Program
     {
+        static ChromeDriver driver;
         static void Main(string[] args)
         {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+
+            IWebElement campoLogin = driver.FindElement(By.Name("email"));
+            IWebElement campoSenha = driver.FindElement(By.Name("pass"));
+
+            campoLogin.SendKeys("nelsontecti@gmail.com");
+            campoSenha.SendKeys("Bankai09");
+
+            campoLogin.Submit();
+
+            driver.Close();
+            driver.Dispose();
         }
     }
 }
